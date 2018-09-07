5e25fe05fc9c434a95d36630cf6b398ce748986e
Jason To <jasto@ucdavis.edu>
2018-09-07 22:32:19 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,12 @@
+function forLoop(array){
+  for (let i = 0; i < 25; i++) {
+  if (i == 1){
+    var additional = "I am 1 strange loop";
+    array.push(additional);
+  } else{
+    var second_addition = "I am ${i} strange loops";
+    array.push(second_addition);
+    }
+  return array;
+  }
+}
