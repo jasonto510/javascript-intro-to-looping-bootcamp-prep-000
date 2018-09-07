cc27b2d166c859b87c65bf0ee9c59a5ab0a35d65
Jason To <jasto@ucdavis.edu>
2018-09-07 22:34:07 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 function forLoop(array){
   for (let i = 0; i < 25; i++) 
   if (i == 1){
-    var additional = "I am 1 strange loop";
+    var additional = "I am 1 strange loop.";
     array.push(additional);
   } else{
     var second_addition = 'I am ${i} strange loops'
